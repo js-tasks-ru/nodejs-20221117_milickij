3d2662a66170423ea2e2b2a12e103ac0ee61d6e2
Alexandr Milickij <amilickij@dpkapp.ru>
2022-12-04 17:17:46 +0300

    add solution 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -13,6 +14,28 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'GET':
 
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('Bad request');
+         break;
+      }
+
+      if (!fs.existsSync(filepath)) {
+        res.statusCode = 404;
+        res.end('Not found');
+        break;
+      }
+
+      const stream = fs.createReadStream(filepath);
+
+      stream.on('error', function (error) {
+        res.statusCode = 500;
+        res.end('Internal error');
+      })
+
+      req.on('aborted', () => stream.destroy());
+      
+      stream.pipe(res);
       break;
 
     default:
