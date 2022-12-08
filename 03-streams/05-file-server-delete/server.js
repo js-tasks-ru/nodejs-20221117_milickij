5f73640a31023466871f3fac82010a217988e567
Alexandr Milickij <amilickij@dpkapp.ru>
2022-12-09 00:29:15 +0300

    add solution 3-module 5-task

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,6 +13,29 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+
+      try {
+        if (pathname.includes('/')) {
+          res.statusCode = 400;
+          res.end('Bad request');
+          return;
+        }
+
+        if (fs.existsSync(filepath)) {
+          fs.unlink(filepath, () => {
+            res.statusCode = 200;
+            res.end('ok');
+          });
+          return;
+        } else {
+          res.statusCode = 404;
+          res.end('Not found');
+          return;
+        }
+      } catch (error) {
+        res.statusCode = 500;
+        res.end('Internal error')
+      }
 
       break;
 
